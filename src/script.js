06b7154611e8e27ecd45f815e811749efe8f4bc3
Tri <trimpham98@gmail.com>
2023-05-27 22:09:38 -0700

    Add custom buffer geometry for a triangle

--- src/script.js
+++ src/script.js
@@ -11,10 +11,18 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+
+const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute('position', positionsAttribute);
+
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
-  wireframe: true
+  wireframe: true,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
